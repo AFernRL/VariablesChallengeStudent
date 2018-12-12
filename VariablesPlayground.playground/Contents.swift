7f7e6e0948fa9756979c608e2a16b87fbfca34e9
ANGEL FERNANDEZ <rl240661@rlas-macair-32.local>
2018-12-12 11:35:59 -0600

    Initial Commit

---
+++ VariablesPlayground.playground/Contents.swift
@@ -0,0 +1,31 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var NumberOfStudentsInClass = 30
+let numberOfPenniesInDime = 10
+//variables can change, constants cant change
+
+var someVariable = true // booleans are either true or false
+
+var passingPeriodMinutes = 5
+var classPeriodMinutes = 45
+var totalClassPeriodMinutes = passingPeriodMinutes + classPeriodMinutes
+
+var typeInteger :Int = 5 //Int is a whole number between -infinity and +infinity
+var typeDouble :Double = 5.2 //Doubles are decimals
+var typeFloat :Float = 5.2//Also a decimal, but use Double instead
+var typeString = "My name is Juan."//A string is text within quotations
+var typeBoolean :Bool = true//Booleans are either True or False
+
+var inferredTypeInt = 5
+var inferredTypeDouble = 5.2
+var inferredTypeString = "My name is Juan."
+var inferredTypeBoolean = true
+
+var age = 17
+print("I am \(age)")
+
+var inferredIntArray = [1, 2, 3, 4, 5]
